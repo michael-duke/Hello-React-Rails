8923f9bcd78cb821855fd7755b99320ab90968a9
michael-duke <michael.21duke@gmail.com>
2023-01-03 01:11:02 +0300

    Set up Greeting component as a Route

--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
@@ -7,7 +8,9 @@ import App from './App';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
+  </Router>
 );
